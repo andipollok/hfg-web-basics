1e850b64ecd9ba710da067fd9839074339c28626
Andi Pollok <mail@andipollok.com>
2023-05-17 22:16:52 +0200

    Fixed some bugs

--- game/main.js
+++ game/main.js
@@ -1,35 +1,38 @@
 var currentPlayer;
 var data;
+var gameRunning;
 
 // 0 = empty
 // 1 = player 1
 // 2 = player 2
 
 function setup() {
-  currentPlayer = 1;
   data = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0],
   ];
-  updateCurrentPlayer();
-  // var playarea = document.querySelector("#playarea");
 
-  // for (var row = 0; row < 3; row++) {
-  //   for (var col = 0; col < 3; col++) {
-  //     var field = document.createElement("div");
-  //     field.setAttribute("id", "field-" + col + "-" + row);
-  //     //field.innerHTML = "Field<br> Col " + col + "<br> Row " + row;
-  //     field.setAttribute("data-row", row);
-  //     field.setAttribute("data-col", col);
-  //     field.onclick = clickField;
-  //     playarea.appendChild(field);
-  //   }
-  // }
+  currentPlayer = 1;
+  updateCurrentPlayer();
+  document.querySelector("#currentplayerbox").style = "display: flex";
+  document.querySelector("#ondraw").style = "display:none";
+  document.querySelector("#onwin").style = "display:none";
+
+  var fieldElements = document.querySelectorAll("#playarea *");
+  fieldElements.forEach((element) => {
+    element.classList.remove("player1");
+    element.classList.remove("player2");
+  });
+
+  gameRunning = true;
 }
+
 function updateCurrentPlayer() {
-  document.querySelector("#currentplayer").classList.value =
-    "player" + currentPlayer;
+  var elements = document.querySelectorAll("[data-currentplayer]");
+  elements.forEach((element) => {
+    element.classList.value = "player" + currentPlayer;
+  });
 }
 
 function clickField(e) {
@@ -37,12 +40,94 @@ function clickField(e) {
   var row = clickedField.getAttribute("data-row");
   var col = clickedField.getAttribute("data-col");
 
-  if (data[row][col] == 0) {
+  if (gameRunning && data[row][col] == 0) {
     data[row][col] = currentPlayer;
     clickedField.classList.add("player" + currentPlayer);
-    // clickedField.innerHTML = currentPlayer;
-
-    currentPlayer = currentPlayer == 1 ? 2 : 1;
-    updateCurrentPlayer();
+    var fieldsWinner = checkWin();
+    if (fieldsWinner) {
+      // one player has won
+      gameRunning = false;
+      document.querySelector("#currentplayerbox").style = "display:none";
+      document.querySelector("#onwin").style = "display:block";
+    } else if (checkIfDraw()) {
+      // draw
+      document.querySelector("#currentplayerbox").style = "display:none";
+      document.querySelector("#ondraw").style = "display:block";
+    } else {
+      // switch player
+      currentPlayer = currentPlayer == 1 ? 2 : 1;
+      updateCurrentPlayer();
+    }
   }
+}
+
+function checkIfDraw() {
+  for (var row = 0; row < 3; row++) {
+    for (var col = 0; col < 3; col++) {
+      if (data[row][col] == 0) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function checkWin() {
+  // check rows
+  for (var row = 0; row < 3; row++) {
+    if (
+      data[row][0] == currentPlayer &&
+      data[row][1] == currentPlayer &&
+      data[row][2] == currentPlayer
+    ) {
+      return [
+        [row, 0],
+        [row, 1],
+        [row, 2],
+      ];
+    }
+  }
+
+  // check columns
+  for (var col = 0; col < 3; col++) {
+    if (
+      data[0][col] == currentPlayer &&
+      data[1][col] == currentPlayer &&
+      data[2][col] == currentPlayer
+    ) {
+      return [
+        [0, col],
+        [1, col],
+        [2, col],
+      ];
+    }
+  }
+
+  // Diagonal 1
+  if (
+    data[0][0] == currentPlayer &&
+    data[1][1] == currentPlayer &&
+    data[2][2] == currentPlayer
+  ) {
+    return [
+      [0, 0],
+      [1, 1],
+      [2, 2],
+    ];
+  }
+
+  // Diagonal 2
+  if (
+    data[2][0] == currentPlayer &&
+    data[1][1] == currentPlayer &&
+    data[0][2] == currentPlayer
+  ) {
+    return [
+      [2, 0],
+      [1, 1],
+      [0, 2],
+    ];
+  }
+  // no win
+  return false;
 }
